be1c62241affe1af85b1b7d3853cc701b1dfff4f
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 08:20:38 -0300

    feat: add deleteUser to IUsersRepository

--- apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
+++ apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
@@ -5,6 +5,7 @@ import { User } from '~/modules/users/infra/entities/user';
 interface IUsersRepository {
   createUser({ nome, email, senha }: ICreateUserDTO): Promise<User>;
   updateUser({ id, nome, email, senha }: IUpdateUserDTO): Promise<User>;
+  deleteUser(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
