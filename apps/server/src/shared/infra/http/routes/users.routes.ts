8aaa3265061134df7b4d5dffdba7f85ff725d68b
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 19:58:04 -0300

    feat: add list users route

--- apps/server/src/shared/infra/http/routes/users.routes.ts
+++ apps/server/src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { CreateUserController } from '~/modules/users/useCases/createUser/CreateUserController';
 import { DeleteUserController } from '~/modules/users/useCases/deleteUser/DeleteUserController';
+import { ListUsersByPaginationController } from '~/modules/users/useCases/listUsersByPagination/ListUsersByPaginationController';
 import { UpdateUserController } from '~/modules/users/useCases/updateUser/UpdateUserController';
 import { Router } from 'express';
 
@@ -10,10 +11,13 @@ const usersRouters = Router();
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserController();
 const deleteUserController = new DeleteUserController();
+const listUsersByPaginationController = new ListUsersByPaginationController();
 
 usersRouters.post('/create', validateUser, createUserController.handle);
 
 usersRouters.put('/update/:id', validateUser, updateUserController.handle);
+
+usersRouters.get('/list', listUsersByPaginationController.handle);
 
 usersRouters.delete('/delete/:id', deleteUserController.handle);
 
