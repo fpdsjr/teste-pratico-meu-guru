a97b3d210925f0d97d69f7898df609705cf6fa04
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 06:31:14 -0300

    feat: add updateUser function to IUsersRepository

--- apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
+++ apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
@@ -1,8 +1,10 @@
 import { ICreateUserDTO } from '~/modules/users/dtos/ICreateUserDTO';
+import { IUpdateUserDTO } from '~/modules/users/dtos/IUpdateUserDTO';
 import { User } from '~/modules/users/infra/entities/user';
 
 interface IUsersRepository {
   createUser({ nome, email, senha }: ICreateUserDTO): Promise<User>;
+  updateUser({ nome, email, senha }: IUpdateUserDTO): Promise<User>;
 }
 
 export { IUsersRepository };
