5feebefc9f50c832507b28ed4c3973f8701e0e8e
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 08:33:27 -0300

    feat: add DeleteUserController

---
+++ apps/server/src/modules/users/useCases/deleteUser/DeleteUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { DeleteUserUseCase } from './DeleteUserUseCase';
+
+class DeleteUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteUserUseCase = container.resolve(DeleteUserUseCase);
+
+    const deleteUser = await deleteUserUseCase.execute(id);
+
+    return response.status(200).json(deleteUser);
+  }
+}
+
+export { DeleteUserController };
