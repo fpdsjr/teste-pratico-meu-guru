646230e0b4597bfea1b7fe009994fb3e15f9a441
fpdsjr <flaviopds.jr@gmail.com>
2022-06-16 23:21:13 -0300

    feat: add UsersRepositoryInMemory createUser

---
+++ server/src/modules/users/infra/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,24 @@
+import { ICreateUserDTO } from '~/modules/users/dtos/ICreateUserDTO';
+
+import { User } from '../../entities/user';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  usersRepository: User[] = [];
+
+  async createUser({ nome, email, senha }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, {
+      nome,
+      email,
+      senha,
+    });
+
+    this.usersRepository.push(user);
+
+    return user;
+  }
+}
+
+export { UsersRepositoryInMemory };
