3a4e1d22114ec5230d69054fb8d4c9e7a299ae63
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 19:56:44 -0300

    feat: add listUsers  functions with and without filters

--- apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
+++ apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
@@ -2,10 +2,14 @@ import { ICreateUserDTO } from '~/modules/users/dtos/ICreateUserDTO';
 import { IUpdateUserDTO } from '~/modules/users/dtos/IUpdateUserDTO';
 import { User } from '~/modules/users/infra/entities/user';
 
+import { IListUsersByPaginationDTO } from '../../dtos/IListUsersByPaginationDTO';
+
 interface IUsersRepository {
   createUser({ nome, email, senha }: ICreateUserDTO): Promise<User>;
   updateUser({ id, nome, email, senha }: IUpdateUserDTO): Promise<User>;
   deleteUser(id: string): Promise<User>;
+  listUsersByPagination({ page, limit, param, filter }: IListUsersByPaginationDTO): Promise<User[]>;
+  listUsersWithoutFilter({ page, limit }: { page: number; limit: number }): Promise<User[]>;
 }
 
 export { IUsersRepository };
