47bfbfd4bbe6d1d512666028d676482080f6ceb4
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 06:54:39 -0300

    feat: add updateUser route

--- apps/server/src/shared/infra/http/routes/users.routes.ts
+++ apps/server/src/shared/infra/http/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { CreateUserController } from '~/modules/users/useCases/createUser/CreateUserController';
+import { UpdateUserController } from '~/modules/users/useCases/updateUser/UpdateUserController';
 import { Router } from 'express';
 
 import { validateUser } from '../middlewares/validateUser';
@@ -6,7 +7,10 @@ import { validateUser } from '../middlewares/validateUser';
 const usersRouters = Router();
 
 const createUserController = new CreateUserController();
+const updateUserController = new UpdateUserController();
 
 usersRouters.post('/create', validateUser, createUserController.handle);
+
+usersRouters.put('/update/:id', validateUser, updateUserController.handle);
 
 export { usersRouters };
