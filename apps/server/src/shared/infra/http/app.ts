059d62e6ce0879091687bcff93b8ee0d764625e8
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 11:43:15 -0300

    feat: set up cors

--- apps/server/src/shared/infra/http/app.ts
+++ apps/server/src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 
@@ -9,6 +10,8 @@ import { router } from './routes';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(router);
 
