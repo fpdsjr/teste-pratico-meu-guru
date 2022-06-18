880662854f510822519462e8171e151eccbe8046
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 10:28:30 -0300

    feat: implement inMemory listUsers functions

--- apps/server/src/modules/users/infra/repositories/in-memory/UsersRepositoryInMemory.ts
+++ apps/server/src/modules/users/infra/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,7 +1,7 @@
 import { ICreateUserDTO } from '~/modules/users/dtos/ICreateUserDTO';
+import { IListUsersByPaginationDTO } from '~/modules/users/dtos/IListUsersByPaginationDTO';
 import { IUpdateUserDTO } from '~/modules/users/dtos/IUpdateUserDTO';
 import { User } from '~/modules/users/infra/entities/user';
-import { AppError } from '~/shared/errors/AppError';
 
 import { IUsersRepository } from '../IUsersRepository';
 
@@ -49,6 +49,30 @@ class UsersRepositoryInMemory implements IUsersRepository {
     const deleteUser = { ...findUser, deletado: true };
 
     return deleteUser;
+  }
+
+  async listUsersWithoutFilter({ page, limit }: { page: number; limit: number }): Promise<User[]> {
+    const spliceUsersArray = this.usersRepository.slice(page * 10, limit);
+
+    return spliceUsersArray;
+  }
+
+  async listUsersByPagination({ page, limit, filter, search }: IListUsersByPaginationDTO): Promise<User[]> {
+    const undefinedMatch = 'undefined' as 'email' | 'nome';
+
+    if (filter === undefinedMatch && search === undefinedMatch) {
+      return this.listUsersWithoutFilter({ page, limit });
+    }
+
+    const filterUsers = this.usersRepository.filter(users => users[filter] === search);
+
+    if (filterUsers.length === 0) {
+      throw new Error();
+    }
+
+    const spliceUsersArray = filterUsers.splice(page * 10, limit);
+
+    return spliceUsersArray;
   }
 }
 
