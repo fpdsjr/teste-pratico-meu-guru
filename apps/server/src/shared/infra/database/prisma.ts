4ee2e7c4e4936fc165856907b17c6d884fe475f4
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 19:49:59 -0300

    refactor: delete soft delete comments

--- apps/server/src/shared/infra/database/prisma.ts
+++ apps/server/src/shared/infra/database/prisma.ts
@@ -14,15 +14,9 @@ const url = DB_URLS[ENV as keyof typeof DB_URLS];
 const prisma = new PrismaClient({ datasources: { db: { url } } });
 
 async function main() {
-  /** ******************************** */
-  /* SOFT DELETE MIDDLEWARE */
-  /** ******************************** */
   prisma.$use(async (params, next) => {
-    // Check incoming query type
     if (params.model === 'Users') {
       if (params.action === 'delete') {
-        // Delete queries
-        // Change action to an update
         params.action = 'update';
         params.args.data = { deletado: true };
       }
