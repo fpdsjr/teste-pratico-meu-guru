4746c92bde6a4a0ef8b05fc981024770e9fe4333
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 02:48:29 -0300

    refactor: add interface and refactor types

--- apps/server/src/modules/users/useCases/listUsersByPagination/ListUsersByPaginationController.ts
+++ apps/server/src/modules/users/useCases/listUsersByPagination/ListUsersByPaginationController.ts
@@ -3,17 +3,24 @@ import { container } from 'tsyringe';
 
 import { ListUsersByPaginationUseCase } from './ListUsersByPaginationUseCase';
 
+interface IConvertQueries {
+  pageToNumber: number;
+  limitToNumber: number;
+  filterToString: string;
+  searchToString: 'email' | 'nome';
+}
+
 class ListUsersByPaginationController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { page, limit, filter, search } = request.query;
-    console.log(search);
 
     const convertQueryTypeParams = {
       pageToNumber: Number(page),
       limitToNumber: Number(limit),
-      filterToString: String(filter),
-      searchToString: String(search),
-    };
+      filterToString: filter,
+      searchToString: search,
+    } as IConvertQueries;
+
     const { pageToNumber, limitToNumber, searchToString, filterToString } = convertQueryTypeParams;
 
     const listUsersByPaginationUseCase = container.resolve(ListUsersByPaginationUseCase);
