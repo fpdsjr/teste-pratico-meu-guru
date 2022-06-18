8d410bc0b26673de4ed92e2de1c43bef0144f5ab
fpdsjr <flaviopds.jr@gmail.com>
2022-06-17 23:26:44 -0300

    refactor: modify order and param name to search for semantic purpose

--- apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
+++ apps/server/src/modules/users/infra/repositories/IUsersRepository.ts
@@ -8,7 +8,7 @@ interface IUsersRepository {
   createUser({ nome, email, senha }: ICreateUserDTO): Promise<User>;
   updateUser({ id, nome, email, senha }: IUpdateUserDTO): Promise<User>;
   deleteUser(id: string): Promise<User>;
-  listUsersByPagination({ page, limit, param, filter }: IListUsersByPaginationDTO): Promise<User[]>;
+  listUsersByPagination({ page, limit, filter, search }: IListUsersByPaginationDTO): Promise<User[]>;
   listUsersWithoutFilter({ page, limit }: { page: number; limit: number }): Promise<User[]>;
 }
 
