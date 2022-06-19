e0b264d75a74ccb61c2dc55b3a183845a427b25d
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 09:09:44 -0300

    feat: add UserProvider to app component

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { GlobalStyle } from '~/styles/global';
+import { UserProvider } from '~/context/UserProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <GlobalStyle />
-      <Component {...pageProps} />;
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />;
+      </ChakraProvider>
+    </UserProvider>
   );
 }
 
