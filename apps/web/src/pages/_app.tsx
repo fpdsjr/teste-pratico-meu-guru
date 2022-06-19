f3ea7feb6a8c8e1004ec2a63b902eb1c42bfd18a
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 04:12:03 -0300

    feat: add charka provide to app component

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import { GlobalStyle } from '~/styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <GlobalStyle />
       <Component {...pageProps} />;
-    </>
+    </ChakraProvider>
   );
 }
 
