27fd9eea263dd7880b5e3358ebd32db117c9462a
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 01:43:45 -0300

    feat: add Header component

---
+++ apps/web/src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1120px;
+  height: 5rem;
+  width: 100%;
+  color: var(--white);
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+`;
