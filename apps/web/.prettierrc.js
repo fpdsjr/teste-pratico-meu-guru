3c4936bd6aae2f2e91f096afa451dcdf90ac5ec6
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 22:35:07 -0300

    chore: add prettier

---
+++ apps/web/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+};
