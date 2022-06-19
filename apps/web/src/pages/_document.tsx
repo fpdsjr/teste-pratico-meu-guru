f04ff35c3fa5aa2db58604976f2987c271cae1b2
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 01:47:10 -0300

    feat: add preconnect

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
           <title>MeuGuru</title>
         </Head>
