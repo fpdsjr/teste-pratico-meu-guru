0254d5e5df43e9a399a2bca9438bfeed807e1e97
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 10:52:38 -0300

    feat: add logo width

--- apps/web/src/components/Header/styles.ts
+++ apps/web/src/components/Header/styles.ts
@@ -6,6 +6,11 @@ export const Container = styled.div`
   width: 100%;
   color: var(--white-100);
   margin: 0 auto;
-  padding: 2rem;
+  padding: 1rem;
   text-align: center;
+
+  img {
+    margin: 0 auto;
+    width: 200px;
+  }
 `;
