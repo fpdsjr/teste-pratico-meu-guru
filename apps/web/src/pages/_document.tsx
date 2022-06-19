a26fc21c9733f95377e0bd43913aaa2994a0fef1
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 10:52:00 -0300

    feat: add meuGuru favicon

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -8,6 +8,11 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            href="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.meuguru.net/&size=16"
+          />
           <title>MeuGuru</title>
         </Head>
         <body>
