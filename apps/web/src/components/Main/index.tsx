4eb6527ba34b369e98a87acffce57675a986c412
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 09:17:55 -0300

    feat: implement table using api data

--- apps/web/src/components/Main/index.tsx
+++ apps/web/src/components/Main/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
 
 import { Container } from './styles';
+import { useUsers } from '~/context/UserProvider/useUser';
+
+interface IUser {
+  id: string;
+  nome: string;
+  email: string;
+  deletado: boolean;
+}
 
 function Main() {
+  const { users } = useUsers();
+
   return (
     <Container>
       <TableContainer>
@@ -11,28 +21,23 @@ function Main() {
           <TableCaption>Imperial to metric conversion factors</TableCaption>
           <Thead>
             <Tr>
-              <Th>To convert</Th>
-              <Th>into</Th>
-              <Th isNumeric>multiply by</Th>
+              <Th>ID</Th>
+              <Th>NOME</Th>
+              <Th>EMAIL</Th>
+              <Th>EXCLUÍDO</Th>
             </Tr>
           </Thead>
-          <Tbody>
-            <Tr>
-              <Td>inches</Td>
-              <Td>millimetres (mm)</Td>
-              <Td isNumeric>25.4</Td>
-            </Tr>
-            <Tr>
-              <Td>feet</Td>
-              <Td>centimetres (cm)</Td>
-              <Td isNumeric>30.48</Td>
-            </Tr>
-            <Tr>
-              <Td>yards</Td>
-              <Td>metres (m)</Td>
-              <Td isNumeric>0.91444</Td>
-            </Tr>
-          </Tbody>
+          {users &&
+            users.map(({ id, nome, email, deletado }: IUser) => (
+              <Tbody key={id}>
+                <Tr>
+                  <Td>{id}</Td>
+                  <Td>{nome}</Td>
+                  <Td>{email}</Td>
+                  <Td>{deletado ? 'TRUE' : 'FALSE'}</Td>
+                </Tr>
+              </Tbody>
+            ))}
         </Table>
       </TableContainer>
     </Container>
