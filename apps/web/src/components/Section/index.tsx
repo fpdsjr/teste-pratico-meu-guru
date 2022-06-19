78655c9f5f4cc48501f8f22192cda81b0107ed7f
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 09:17:30 -0300

    feat: add inputs and functions

--- apps/web/src/components/Section/index.tsx
+++ apps/web/src/components/Section/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useUsers } from '~/context/UserProvider/useUser';
 
 import { Container } from './styles';
 
 function Section() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectOption, setSelectOption] = useState('nome');
+  const { handleFetchWithFilters, handleResetFilters } = useUsers();
+
+  async function handleClick() {
+    handleFetchWithFilters(selectOption, inputValue);
+    setInputValue('');
+  }
+
   return (
     <Container>
-      <input type="text" />
-      <select>
-        <option>nome</option>
-        <option>email</option>
+      <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} />
+      <select value={selectOption} onChange={e => setSelectOption(e.target.value)}>
+        <option value="nome">nome</option>
+        <option value="email">email</option>
       </select>
-      <button type="button">Filtrar</button>
-      <button type="button">Reset</button>
+      <button type="button" onClick={handleClick}>
+        Filtrar
+      </button>
+      <button type="button" onClick={handleResetFilters}>
+        Reset
+      </button>
     </Container>
   );
 }
