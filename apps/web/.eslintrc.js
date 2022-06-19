d0135cdcc5b64acf26a1c51a31cc35b367d24281
fpdsjr <flaviopds.jr@gmail.com>
2022-06-18 22:27:45 -0300

    chore: add eslint

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:import/typescript', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+      'space-before-function-paren': 'off',
+      'react/prop-types': 'off',
+      'import/no-unresolved': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'import/prefer-default-export': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'camelcase': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.ts', '**/*.spec.ts', '**/*.stories.tsx'] }],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+    },
+    settings: {
+      react: {
+        pragma: 'React',
+        version: 'detect',
+      },
+    },
+  };  
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello World</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
