80e21eccc72ab5465e2deb5a206ba807226408bf
fpdsjr <flaviopds.jr@gmail.com>
2022-06-19 04:12:20 -0300

    feat: add Main component

---
+++ apps/web/src/components/Main/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 0 auto;
+  color: white;
+  font-weight: 700;
+  background-color: #03bb967a;
+  border-radius: 0.5rem;
+
+  table {
+    margin: 0 auto;
+
+    th {
+      color: #2a004f;
+      border-radius: 0.25rem;
+    }
+
+    tr {
+      border-radius: 5rem;
+    }
+  }
+`;
