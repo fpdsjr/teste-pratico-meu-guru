5705509c5e60cf27d1107718cc7ad92903f106f3
fpdsjr <flaviopds.jr@gmail.com>
2022-06-20 01:18:57 -0300

    refactor: change logo alt text

--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles';
 function Header() {
   return (
     <Container>
-      <img src="https://www.meuguru.net/_next/static/media/logo.1a16ba5a.svg" alt="logoImage" />
+      <img src="https://www.meuguru.net/_next/static/media/logo.1a16ba5a.svg" alt="logoMeuGuru" />
     </Container>
   );
 }
